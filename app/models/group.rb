908d71016f24691ef5eaa94decd91ca094b31f90
Tekhlay <teklaybirhane12@yahoo.com>
2023-05-31 02:24:23 +0300

    Fix linter issues

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,6 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :expense_groups, dependent: :destroy
   has_many :expenses, through: :expense_groups
- 
 
   validates :name, presence: true, length: { minimum: 3, maximum: 250 }
   validates :icon, presence: true, length: { minimum: 3, maximum: 250 }
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,43 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-    
-    before :each do
-        @user = User.create(name: 'Tekhaly', email: 'tek@gmail.com', 
-        password: '123456', password_confirmation: '123456')
-        @group = Group.create(name: 'Food', author: @user)
-        @expense = Expense.create(name: 'Food', amount: 100, author_id: @user)
-    end
+  before :each do
+    @user = User.create(name: 'Tekhaly', email: 'tek@gmail.com',
+                        password: '123456', password_confirmation: '123456')
+    @group = Group.create(name: 'Food', author: @user)
+    @expense = Expense.create(name: 'Food', amount: 100, author_id: @user)
+  end
 
-    it 'should have valid name' do
-        @expense.name = nil
-        expect(@expense).to_not be_valid
-    end
+  it 'should have valid name' do
+    @expense.name = nil
+    expect(@expense).to_not be_valid
+  end
 
-    it 'should have name greater than 3 characters' do
-        @expense.name = 'Fo'
-        expect(@expense).to_not be_valid
-    end
+  it 'should have name greater than 3 characters' do
+    @expense.name = 'Fo'
+    expect(@expense).to_not be_valid
+  end
 
-    it 'should have name less than 250 characters' do
-        @expense.name = 'a' * 251
-        expect(@expense).to_not be_valid
-    end
+  it 'should have name less than 250 characters' do
+    @expense.name = 'a' * 251
+    expect(@expense).to_not be_valid
+  end
 
-    it 'should have valid author' do
-        @expense.author = nil
-        expect(@expense).to_not be_valid
-    end
+  it 'should have valid author' do
+    @expense.author = nil
+    expect(@expense).to_not be_valid
+  end
 
-    it 'should have valid amount' do
-        @expense.amount = nil
-        expect(@expense).to_not be_valid
-    end
+  it 'should have valid amount' do
+    @expense.amount = nil
+    expect(@expense).to_not be_valid
+  end
 
-    it 'should have amount greater than 0' do
-        @expense.amount = 0
-        expect(@expense).to_not be_valid
-    end
-
-
+  it 'should have amount greater than 0' do
+    @expense.amount = 0
+    expect(@expense).to_not be_valid
+  end
 end
